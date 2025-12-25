4d1c29d1185b16f946459d19d21c642b9afad71c
Chey Kimirth <cheykimrith123@gmail>
2025-12-26 02:57:51 +0700

    fix product upload folder

--- src/Routes/product.route.js
+++ src/Routes/product.route.js
@@ -1,14 +1,25 @@
 const product = require("../Controller/product.controller");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 // ============================
 // MULTER STORAGE
 // ============================
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads"),
-  filename: (req, file, cb) =>
-    cb(null, Date.now() + path.extname(file.originalname)),
+  destination: (req, file, cb) => {
+    const dir = "uploads/products";
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage });
